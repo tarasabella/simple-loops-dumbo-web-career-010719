df5d5513db854644463f796f70121f1a85a7cacf
Tara Sabella <tarasabella@gmail.com>
2020-11-12 06:56:45 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -22,7 +22,7 @@ end
 def while_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   counter = 0 
-  while counter < number_of_times
+  while counter <= number_of_times
     puts phrase 
     counter += 1
   end
